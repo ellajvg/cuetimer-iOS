6f0ca044298410a1eefcf83ec8e7f79e0c3a11ef
ellajvg <ellajvg@gmail.com>
2025-01-22 20:58:33 -0800

    Initial Commit

---
+++ CueTimer/CueTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CueTimerApp.swift
+//  CueTimer
+//
+//  Created by Ella Vanderkop-Girard on 2025-01-22.
+//
+
+import SwiftUI
+
+@main
+struct CueTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
