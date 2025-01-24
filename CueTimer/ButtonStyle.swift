23a1dfef052505818cfb1dc0eeb6fa2fa503e486
Ella Vanderkop-Girard <151410530+ellajvg@users.noreply.github.com>
2025-01-24 13:02:28 -0800

    Universal button style, mainpage, and app intro animation created

--- CueTimer/ButtonStyle.swift
+++ CueTimer/ButtonStyle.swift
@@ -5,4 +5,27 @@
 //  Created by Ella Vanderkop-Girard on 2025-01-23.
 //
 
-import Foundation
+import SwiftUI
+
+struct ButtonStyle: ViewModifier {
+    let foregroundStyle: Color
+    let background: Color
+    private let shadowColor: Color = .black
+
+    func body(content: Content) -> some View {
+        content
+            .font(.title2)
+            .fontWeight(.bold)
+            .foregroundStyle(foregroundStyle)
+            .padding()
+            .background(background)
+            .cornerRadius(10)
+            .shadow(color: shadowColor, radius: 5, y: 5)
+    }
+}
+
+extension View {
+    func buttonStyle(foregroundStyle: Color, background: Color) -> some View {
+        self.modifier(ButtonStyle(foregroundStyle: foregroundStyle, background: background))
+    }
+}
--- CueTimer/ContentView.swift
+++ CueTimer/ContentView.swift
@@ -10,12 +10,61 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                Image(systemName: "house.fill")
+                Spacer()
+                Image(systemName: "person.fill")
+            
+            }
+            .font(.largeTitle)
+            .fontWeight(.bold)
+            .padding(.horizontal, 30)
+            .foregroundColor(Color.accentColor)
+            
+            Image("Logo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding(.top, 60)
+                .padding(.bottom, 60)
+                .padding(.horizontal, 100)
+            
+            VStack {
+                Spacer()
+                Button("TIMER") {
+                        
+                }.buttonStyle(
+                    foregroundStyle: .white,
+                    background: Color.accentColor
+                )
+                Spacer()
+                    
+                Button("EXERCISE CUES") {
+                        
+                }.buttonStyle(
+                    foregroundStyle: .white,
+                    background: Color.accentColor
+                )
+                Spacer()
+                    
+                Button("CUES + TIMER") {
+                        
+                }
+                .buttonStyle(
+                    foregroundStyle: .black,
+                    background: .white
+                )
+                Spacer()
+            }
+            .padding(30)
+            .padding(.top, 30)
+            
         }
-        .padding()
+        .background(
+            Image("Background")
+                .ignoresSafeArea()
+                .offset(y:250)
+        )
+        
     }
 }
 
