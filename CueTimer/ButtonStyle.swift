c329cea06075a7e1ad9a7cad87e083145324206b
Ella Vanderkop-Girard <151410530+ellajvg@users.noreply.github.com>
2025-01-24 12:54:27 -0800

    Accent color added

---
+++ CueTimer/ButtonStyle.swift
@@ -0,0 +1,8 @@
+//
+//  ButtonStyle.swift
+//  CueTimer
+//
+//  Created by Ella Vanderkop-Girard on 2025-01-23.
+//
+
+import Foundation
---
+++ CueTimer/Intro.swift
@@ -0,0 +1,8 @@
+//
+//  Intro.swift
+//  CueTimer
+//
+//  Created by Ella Vanderkop-Girard on 2025-01-24.
+//
+
+import Foundation
