12d66ffb97ef7a670d10ebdd96bdb3eafcf051e5
Ella Vanderkop-Girard <151410530+ellajvg@users.noreply.github.com>
2025-03-03 22:15:44 -0800

    Major bugs fixed, accounts init

---
+++ CueTimer/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  CueTimer
+//
+//  Created by Ella Vanderkop-Girard on 2025-02-28.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
